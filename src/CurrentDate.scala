7ca1159ac75557206fd4884cb9f9d47610d2fc42
peterse73 <peterse73@gmail.com>
2015-07-16 23:18:27 +0200

    date example

---
+++ src/CurrentDate.scala
@@ -0,0 +1,15 @@
+/**
+ * User: gpe-soft
+ * Date: 16-7-2015    22:28
+ */
+
+import java.util.{Locale, Date}
+import java.text.DateFormat._
+
+object CurrentDate {
+  def main(args: Array[String]) {
+    val now = new Date;
+    val df = getDateInstance(LONG, Locale.US);
+    println("Today's date is: " + df.format(now));
+  }
+}
