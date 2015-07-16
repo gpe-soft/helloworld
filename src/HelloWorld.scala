c3a9c3cc53c7fea3899e8729a7bc300d437e5fe5
peterse73 <peterse73@gmail.com>
2015-07-16 23:19:03 +0200

    added comments

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -2,9 +2,8 @@
  * User: gpe-soft
  * Date: 4-7-2015    22:51
  */
-object HelloWorld {
-  def main(args: Array[String]) {
+object HelloWorld {    // singleton so declare object instead of class
+  def main(args: Array[String]) {   // no return type and no static declaration of the main method
     println("Hello, world!");
-    // comments
   }
 }
